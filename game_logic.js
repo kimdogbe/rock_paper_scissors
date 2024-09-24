dd46e43f46d38feb668f2188b8b7b34f8fc3223d
kimdogbe <kimdogbe@gmail.com>
2024-09-24 15:54:00 +0100

    Add computer choice logic
    Added logic for computer choice of rock paper or scissors.
    Uses Math.Random to select initial number and array to
    select which element.

--- game_logic.js
+++ game_logic.js
@@ -1,2 +1,7 @@
-console.log("Hello kdogg")
+function getComputerChoice() {
+  let choices = ['Rock', 'Paper', 'Scissors'];
+  let randoNumbo = Math.floor(Math.random()*3);
+  return choices[randoNumbo];
+}
 
+console.log(getComputerChoice());
