df432bccc0bc8c00924f8c8867232c7b9996ee8c
kimdogbe <kimdogbe@gmail.com>
2024-09-24 17:20:53 +0100

    Add humanChoice function

--- game_logic.js
+++ game_logic.js
@@ -1,7 +1,20 @@
+let choices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-  let choices = ['Rock', 'Paper', 'Scissors'];
   let randoNumbo = Math.floor(Math.random()*3);
   return choices[randoNumbo];
 }
 
+function getHumanChoice() {
+  let humanChoice = prompt("Rock, Paper or Scissors?");
+
+  if (!(choices.includes(humanChoice.toLowerCase()))){
+    console.log(humanChoice + " not a valid choice. please choose either Rock, Paper or Scissors");
+    getHumanChoice();
+  }
+
+  return humanChoice;
+}
+
 console.log(getComputerChoice());
+getHumanChoice();
