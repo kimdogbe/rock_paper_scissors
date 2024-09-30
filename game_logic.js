418f0efd1cd725b2d1d6f0f95f5f6215e7ed50f1
kimdogbe <kimdogbe@gmail.com>
2024-09-30 11:27:26 +0100

    Update logic to use buttons

--- game_logic.js
+++ game_logic.js
@@ -1,47 +1,60 @@
 const choices = ['rock', 'paper', 'scissors'];
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+
+const buttons = document.querySelector("#choices");
+const resultDiv = document.querySelector(".result");
+
+buttons.addEventListener("click", function(e){
+  const buttonPressed = e.target.id;
+  
+  if (roundNumber <= 5){
+    let cpuChoice = getComputerChoice();
+    playRound(buttonPressed, cpuChoice);
+    roundNumber++;
+  } else {
+    alert("Game complete. Restart Game.");
+    
+  }
+});
 
 function getComputerChoice() {
   let randoNumbo = Math.floor(Math.random()*3);
   return choices[randoNumbo];
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("Rock, Paper or Scissors?");
+function updateResultDiv(message, color){
+  resultDiv.style.backgroundColor = color;
+  resultDiv.textContent = message;
+}
 
-  if (!(choices.includes(humanChoice.toLowerCase()))){
-    console.log(humanChoice + " not a valid choice. please choose either Rock, Paper or Scissors");
-    humanChoice = getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice){
+    updateResultDiv(`Tie! You chose: ${humanChoice}  CPU chose: ${computerChoice}`, "yellow");
+    return "Tie"
   }
 
-  return humanChoice.toLowerCase();
+  if (  (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+  ){
+    humanScore++;
+    updateResultDiv(`You chose: ${humanChoice}  CPU chose: ${computerChoice}`, "green");
+  }
+  else {
+    computerScore++;
+    updateResultDiv(`You chose: ${humanChoice}  CPU chose: ${computerChoice}`, "red");
+  }
 }
 
 function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice){
-      return "Tie"
-    }
-
-    if (  (humanChoice === "rock" && computerChoice === "scissors") ||
-          (humanChoice === "paper" && computerChoice === "rock") ||
-          (humanChoice === "scissors" && computerChoice === "paper")
-    ){
-      humanScore++;
-    }
-    else {
-      computerScore++;
-    }
-  }
-
   for(let i = 1; i < 6; i++){
     console.log(`Round ${i}!`)
 
     let humChoice = getHumanChoice();
     let cpuChoice = getComputerChoice();
-    console.log(`You chose: ${humChoice}  CPU chose: ${cpuChoice}`)
+    console.log(`You chose: ${humChoice}  CPU chose: ${cpuChoice}`);
 
     playRound(humChoice, cpuChoice);
     console.log(`Current score: You: ${humanScore} vs CPU: ${computerScore}`);
@@ -58,4 +71,5 @@ function playGame() {
   }
 }
 
-playGame();
+playRound();
+// playGame();
